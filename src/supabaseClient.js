fd6e8509fe22e391378d4cb2382af5ff0abfae24
viicthor <victorgonzaleznavarro18@gmail.com>
2025-05-16 03:07:54 -0600

    Trigger redeploy

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://luxhnzjexlruhqghdpuq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1eGhuempleGxydWhxZ2hkcHVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczNzM5ODMsImV4cCI6MjA2Mjk0OTk4M30.o9rkocvDQUf77JzqbJSQtB_T8alsLmI8_hmpVm6Z-jw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
